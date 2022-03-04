b04f36cb08dfba32ba7c880610f0eb30f046d567
Marius <marius@example.com>
2022-03-04 11:37:00 +0200

    uzd

--- js/sortAcc.js
+++ js/sortAcc.js
@@ -1,3 +1,8 @@
 // paspaudus ant mygtuko ASC keicias i DESC ir atvirksciai
 
-//
+// kai paspaudiam ant Question 1 parodom po juo esanti elementa,
+// dar karta paspaudus paslepiam (classList.toggle())
+
+// 3. padarom kad paspaudus nat bet kurio Question el butu parodomas po juo esantis el
+
+// 4. padarom kad kai paspaudziam ant elemento, jei yra atsidares kazkuris kitas, tai uzdarom atsidariusius ir paliekam atidaryta tik ta kuri paspaudem.
